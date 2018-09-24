ca863c74d398989e80f8eebb0291af0613c2a66b
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2018-09-24 18:21:35 +0100

    basic styling

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -4,4 +4,11 @@ export default class TodoItems extends Component {
   createTasks(item) {
     return <li key={item.key}>{item.text}</li>;
   }
+
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
+    console.log(this.createTasks);
+    return <ul className="theList">{listItems}</ul>;
+  }
 }
